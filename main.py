24ed467c5e30dc87afa290da19147a344ed48d4a
nushant22 <nushantghimire22@gmail.com>
2026-02-06 16:36:46 +0545

    feat: add Pydantic models and endpoint to fetch all products

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -6,4 +7,11 @@ app = FastAPI()
 def greet():
     return("Hello, World!")
 
+products = [
+    Product(id = 1, name = "Phone", description = "Budget Phones",price = 9999, quantity = 10),
+    Product(id = 2, name= "Laptop", description = "Budget Laptop", price = 59999, quantity = 5)
+]
 
+@app.get("/products")
+def get_all_products():
+    return products
