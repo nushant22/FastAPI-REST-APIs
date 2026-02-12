74b552d84fa11caf476a4c8188e145e3c59e4596
nushant22 <nushantghimire22@gmail.com>
2026-02-12 21:23:27 +0545

    feat: Implement SQLAlchemy ORM models for PostgreSQL database

---
+++ database.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+db_url = os.getenv("db_url")
+engine = create_engine(db_url)
+session = sessionmaker(autocommit = False, autoflush = False, bind = engine)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from models import Product
+from database import session, engine
+import database_models
 
 app = FastAPI()
+
+database_models.Base.metadata.create_all(bind = engine)
 
 @app.get("/")
 def greet():
@@ -14,6 +18,8 @@ products = [
 
 @app.get("/products")
 def get_all_products():
+    db = session()
+    db.query()
     return products
 
 @app.get("/product/{id}")
